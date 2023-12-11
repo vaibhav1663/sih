2ecfb8cadfc598a385634f0e8d6cf7ae7c29248e
Om Patil <omajitsinhpatil@gmail.com>
2023-12-11 19:56:55 +0530

    better file structure for server

---
+++ server/controllers/user/test.js
@@ -0,0 +1,3 @@
+exports.test = (req, res) => {
+  res.send("helll0 from test");
+};
--- server/index.js
+++ server/index.js
@@ -1,52 +1,24 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const app = express();
-const db = "mongodb+srv://username:27bKylHTcLzRZqhf@brocoders.kmwx8fe.mongodb.net/sih"
-
-mongoose.connect(db,{
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-}).then(()=>{
-    console.log("We got the MongoDB.")
-}).catch((err)=>{console.log(err)});
-
-
+const connectDB = require("./config/connectDB");
+const cors = require("cors");
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
-const User = require("./model/userSchema");
 
-const middleware = (req,res,next)=>{
-    console.log("Middleware");
-    next();
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again later",
+});
+const userRouter = require("./routes/user");
+connectDB();
+app.use("/books", limiter);
+app.use("/reviewer", limiter);
+app.use("/user", limiter, userRouter);
 
-}
-
-app.get("/", (req,res)=>{
-    res.send("Hello")
-})
-app.post("/add",async (req,res)=>{
-    const {uid, name, email, age, gender, field, collegeName, degree, year} = req.body;
-    if(!uid || !name || !email || !age || !gender || !field || !collegeName || !degree || !year){
-        return res.status(422).json({error: "Field cant be empty "});
-    }
-    try{
-        const userExists = await User.findOne({uid: uid});
-        if(userExists){
-            return res.status(422).json({error: "Email Exists"})
-        }
-        const user = new User({uid,name, email, age, gender, field, collegeName, degree, year});
-        const userRegister = await user.save();
-        console.log(userRegister);
-        if(userRegister){
-            res.status(201).json({msg: 'Added the user'})
-        }else{
-            res.status(500).json({error: "Internal server error "})
-        }
-    }
-    catch(err){
-        err => {console.log(err)}
-    }
-})
-
-app.listen(5000, ()=>{
-    console.log("Server is runnning..")
-})
+app.listen(5000, () => {
+  console.log("Server is runnning..");
+});
