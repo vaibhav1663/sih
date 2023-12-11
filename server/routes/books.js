f7bec096b388d625ee8f74f185f82f29fd136e9c
Om Patil <omajitsinhpatil@gmail.com>
2023-12-11 22:17:53 +0530

    book response1

--- server/routes/books.js
+++ server/routes/books.js
@@ -3,7 +3,7 @@ const { addBook } = require("../controllers/books/addBookResponse");
 const router = express.Router();
 const { getBooks } = require("../controllers/books/getBooks");
 
-router.post("/addBookResponse", addBook);
+router.post("/addBookReview", addBook);
 router.get("/getBooks", getBooks);
 
 module.exports = router;
