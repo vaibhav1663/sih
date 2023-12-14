132373caaa1f1e547554e457448a4be859a09e26
Payal <payalss2004@gmail.com>
2023-12-14 19:58:45 +0530

    landing-page part1

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router";
 import { useUserAuth } from "../Context/UserAuthContext";
 import Navbar from "./Navbar";
+import Carousel from "./Carousel/Carousel";
+import Info from "./Carousel/Info.jsx";
+import BookCard from "./Carousel/BookCard";
 
 const Home = () => {
     const { logOut, user } = useUserAuth();
@@ -16,7 +19,13 @@ const Home = () => {
     };
     return (
         <>
-            <Navbar page="home"/>
+            <div className="h-screen">
+                <Navbar page="home"/>
+                <Carousel></Carousel>
+            </div>
+            <div className="mt-28">
+                <BookCard></BookCard>
+            </div>
             <div className="p-4 box mt-3 text-center">
                 Hello Welcome <br />
                 {user && user.email}
