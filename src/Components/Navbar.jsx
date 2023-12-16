e5acdc8e9748e25ad0058daf929ac5342950221a
Payal <payalss2004@gmail.com>
2023-12-16 12:20:13 +0530

    navbar minor changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -80,16 +80,6 @@ const Navbar = (props) => {
                         </li>
                         <li>
                             <a
-                                href="/admin"
-                                className={
-                                    props.page === "admin" ? highlight : simple
-                                }
-                            >
-                                Services
-                            </a>
-                        </li>
-                        <li>
-                            <a
                                 href="/book-reviews"
                                 className={
                                     props.page === "book-reviews"
@@ -105,20 +95,12 @@ const Navbar = (props) => {
                                 href="/"
                                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >
-                                Pricing
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="/"
-                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                            >
                                 Contact
                             </a>
                         </li>
                         <li>
                             <a
-                                href="/Admin"
+                                href="/login"
                                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >
                                 Login
