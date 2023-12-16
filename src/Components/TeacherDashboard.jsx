eecaf9dd0e0aad175d08ed9e78efe502a5bda978
adityasd314 <adityasd314@gmail.com>
2023-12-16 14:32:52 +0530

    TeacherDashboard -ReviewTable Added

--- src/Components/TeacherDashboard.jsx
+++ src/Components/TeacherDashboard.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import Navbar from './Navbar'
-
+import React from "react";
+import Navbar from "./Navbar";
+import Table from "./TeacherDashboard/table";
+import { useAuthContext } from "../hooks/useAuthContext";
 const TeacherDashboard = () => {
-    return (
-        <>
-            <Navbar page="admin" />
-            <div className="p-4 box text-center">
-                <h1 className='mt-24 font-bold text-4xl'>Teacher Dashboard</h1>
-                
-            </div>
-        </>
-    )
-}
+  const teacher = useAuthContext();
+  const teacherID = '123' || teacher._id;// '123' is only id availabke for now
 
-export default TeacherDashboard
+  return (
+    <>
+      <Navbar page="admin" />
+      <div className="p-4 box text-center">
+        <h1 className="mt-24 font-bold text-4xl">Teacher Dashboard</h1>
+        <Table id={teacherID}/>
+      </div>
+    </>
+  );
+};
+
+export default TeacherDashboard;
