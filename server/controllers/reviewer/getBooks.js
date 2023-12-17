4ec9edeef22df248ff7d6b8db8be129b907c4b0a
Om Patil <omajitsinhpatil@gmail.com>
2023-12-17 16:35:37 +0530

    reviewer getbook implemented

--- server/controllers/reviewer/getBooks.js
+++ server/controllers/reviewer/getBooks.js
@@ -1,23 +1,40 @@
-const Book = require("../../model/bookSchema");
+const rq = require("../../model/reviewerqueue");
+const book = require("../../model/bookSchema");
 
 exports.getBooksByReviewerId = async (req, res) => {
-  const { id } = req.query;
+  const { _id } = req.body;
 
   try {
-    // Find books where reviewerAlloted array contains the provided id
-    const books = await Book.find({
-      reviewerAlloted: { $in: [id] },
+    // Find reviewer queue based on reviewer ID
+    const reviewerQueue = await rq.findOne({
+      reviewerid: _id,
     });
 
-    // Check if there are no books
-    if (!books || books.length === 0) {
+    // Check if there are no books in the reviewer queue
+    if (!reviewerQueue) {
       return res
         .status(404)
         .json({ error: "No books found for the given reviewer id" });
     }
 
+    const { tobereviewed, reviewed } = reviewerQueue;
+
+    // Fetch details for books in tobereviewed array
+    const tobereviewedBooks = await Promise.all(
+      tobereviewed.map(async (element) => {
+        return await book.findById(element);
+      })
+    );
+
+    // Fetch details for books in reviewed array
+    const reviewedBooks = await Promise.all(
+      reviewed.map(async (element) => {
+        return await book.findById(element);
+      })
+    );
+
     // Return the array of books
-    res.status(200).json(books);
+    res.status(200).json({ tobereviewedBooks, reviewedBooks });
   } catch (error) {
     console.error("Error fetching books:", error);
     res.status(500).json({ error: "Internal server error" });
