c744ab89ca29e4634fb80dcd3b0c89ba61f90810
Payal <payalss2004@gmail.com>
2023-12-18 23:09:18 +0530

    language section added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,6 +8,7 @@ import BookCard from "./Carousel/BookCard";
 import About from "./Carousel/About";
 import Info from "./Carousel/Info";
 import Footer from "./Carousel/Footer";
+import ReviewForm from "./ReviewForm";
 
 let slides=[
     {
@@ -48,6 +49,7 @@ const Home = () => {
             </div>
             <Info></Info>
             <Footer></Footer>
+            <ReviewForm></ReviewForm>
         </>
     );
 };
