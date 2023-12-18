6fb8d6c87bc0e73dc6c1261b89613fe404564d73
TanmayBansode <tanmaysbansode@gmail.com>
2023-12-18 20:03:11 +0530

    book rating fixed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(helmet());
 app.use(express.json());
 
 const limiter = rateLimit({
-    max: 11111111111
+  max: 11111111111,
 });
 const userRouter = require("./routes/user");
 const bookRouter = require("./routes/books");
@@ -25,5 +25,5 @@ app.use("/user", limiter, userRouter);
 app.use("/admin", limiter, adminRouter);
 
 app.listen(5000, () => {
-    console.log("Server is runnning..");
+  console.log("Server is runnning..");
 });
