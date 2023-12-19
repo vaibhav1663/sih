75351cdf3566b003a5fb629ab02fe42fe3e87b3d
Om Patil <omajitsinhpatil@gmail.com>
2023-12-20 01:54:25 +0530

    ai ai ai

--- server/routes/bard.js
+++ server/routes/bard.js
@@ -7,7 +7,11 @@ const {
 const {
   getParameterizedReview,
 } = require("../controllers/bard/getParameterizedReview");
+const {
+  getComparisionReview,
+} = require("../controllers/bard/getComparisionReview");
 router.post("/getGeneralOverview/", getGeneralOverview);
 router.post("/getParameterizedReview/", getParameterizedReview);
+router.post("/getComparisionReview/", getComparisionReview);
 
 module.exports = router;
