fb796c35e021b95051f71fde7741e5e8234842bf
Om Patil <omajitsinhpatil@gmail.com>
2023-12-19 23:44:56 +0530

    revise

--- server/controllers/author/revise.js
+++ server/controllers/author/revise.js
@@ -1,4 +1,8 @@
 const Book = require("../../model/bookSchema");
 const RecommendedBook = require("../../model/recommendedBooks");
 
-exports.reject = async (req, res) => {};
+exports.revise = async (req, res) => {
+   const { _id, name, desc, imageLink, buyLink, previewLink } =
+ // book and recommendedBooks -> find id
+    //reject array techya last ele, date, deadline, add-> todaysDate<addn -> go forward -> update these 5 in both, 
+};
